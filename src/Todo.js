5b8be0a8d69c4ec2997875d0ba8d125e12e03028
badasahog <yairkatz10@gmail.com>
2020-06-18 14:25:02 +0300

    fixed router

--- src/Todo.js
+++ src/Todo.js
@@ -4,24 +4,30 @@ import {
 	Link,
 } from "react-router-dom";
 
-function Todo({ todo, index, completeTodo, removeTodo }) {
+  class Todo extends React.Component{
+	constructor( todo, index, completeTodo, removeTodo) {
+		//super({ todo, index, completeTodo, removeTodo });
+		super();
+		this.state = {color: "red"};
+	}	  
+	render() {
     return (
-    <Router>
-      <div
-        className="todo"
-        style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
-      >
-        <Link to={`/homework/${index}/`} attr="view" className="btn btn-sm btn-link">
-            {todo.text}
-        </Link>
-  
-        <div>
-          <button className="btn btn-sm btn-outline-success" onClick={() => completeTodo(index)}>Complete</button>
-          <button className="btn btn-sm btn-outline-danger ml-1" onClick={() => removeTodo(index)}>x</button>
-        </div>
-      </div>
-      </Router>
+			<div
+			className="todo"
+			style={{ textDecoration: this.props.todo.isCompleted ? "line-through" : "" }}
+			>
+	<Link to={`/homework/${this.props.index}`} >
+					{this.props.todo.text}
+				</Link>
+
+				<div>
+					<button className="btn btn-sm btn-outline-success" onClick={() => this.props.completeTodo(this.props.index)}>Complete</button>
+					<button className="btn btn-sm btn-outline-danger ml-1" onClick={() => this.props.removeTodo(this.props.index)}>x</button>
+				</div>
+			</div>
     );
+  }
+    
   }
 
   export default Todo;
